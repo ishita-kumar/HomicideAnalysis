7e4bc77753f39190dc70f10131707d0f6919b9f0
msureshk <msureshk@iu.edu>
2020-05-05 16:52:04 -0400

    json for chart.js

--- PyMongo Queries/VictimByRaceByYear.py
+++ PyMongo Queries/VictimByRaceByYear.py
@@ -24,14 +24,30 @@ mydoc = Crime.aggregate([
             },
         },
         "CountByRace": { "$sum": "$CountByRace" }
-    }}
+    }},
+    {
+        "$sort" : {"_id" : 1}
+    }
 ])
-
-with open('VictimByRace.json', 'w') as file:
-
-    file.write('[')
+years = []
+race_wise_crime_count = {}
+with open('VictimByRaceAndByYear.json', 'w') as file:
     for document in mydoc:
-        
-        file.write(json.dumps(document))
-        file.write(',\n')
-    file.write(']')
+        if not document["_id"]:
+            continue
+        years.append(int(document["_id"]))
+        for race_count_dict in document["CrimeTypes"]:
+            # print(race_count_dict)
+            race = race_count_dict[list(race_count_dict.keys())[0]]
+            count = race_count_dict[list(race_count_dict.keys())[1]]
+            if race not in race_wise_crime_count:
+                race_wise_crime_count[race] = []
+            race_wise_crime_count[race].append(count)
+            file.write(json.dumps(document))
+            file.write(',\n')
+# print(years)
+# print(race_wise_crime_count)
+with open('VictimByRace.json', 'w') as file2:
+    json.dump(race_wise_crime_count, file2)
+with open('YearAscendingOrder.json', 'w') as file3:
+    json.dump({"Years" : years}, file3)
