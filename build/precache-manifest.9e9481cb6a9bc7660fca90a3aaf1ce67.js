8ee643e0b5f8205fd8ec7d59f752c3a6a8c6df20
knikharg <knikharg@iu.edu>
2020-05-06 15:41:00 -0400

    image changes

---
+++ build/precache-manifest.9e9481cb6a9bc7660fca90a3aaf1ce67.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "276cc82988663fb192b1",
+    "url": "/static/css/main.00ea55d8.chunk.css"
+  },
+  {
+    "revision": "276cc82988663fb192b1",
+    "url": "/static/js/main.276cc829.chunk.js"
+  },
+  {
+    "revision": "732de8973a519ca0f456",
+    "url": "/static/js/1.732de897.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8dc14e466c46b4c4e78d5bba73418a8e",
+    "url": "/static/media/gun.8dc14e46.jpg"
+  },
+  {
+    "revision": "ccef7038ece63f2110113a862d45e6c7",
+    "url": "/static/media/victim3.ccef7038.png"
+  },
+  {
+    "revision": "7cb4b265ff16692466359e41e2a11e78",
+    "url": "/static/media/usmap.7cb4b265.jpg"
+  },
+  {
+    "revision": "fa04f352abfa5fe18b65de7e4dbeca01",
+    "url": "/static/media/race.fa04f352.jpg"
+  },
+  {
+    "revision": "95cdecad9d4013d260d5c97738d63fc4",
+    "url": "/static/media/ishita.95cdecad.jpg"
+  },
+  {
+    "revision": "f52f0915ab8c04477efd9f0b825c0dbe",
+    "url": "/static/media/manisha.f52f0915.jpeg"
+  },
+  {
+    "revision": "b3a79703838616858b733f92da2ef739",
+    "url": "/static/media/kasturi.b3a79703.jpeg"
+  },
+  {
+    "revision": "ffe5fa548a10bfee889feff0a77b8497",
+    "url": "/static/media/man.ffe5fa54.jpg"
+  },
+  {
+    "revision": "8ca8c8e073b33b4db3ba09574880db08",
+    "url": "/static/media/Moon.8ca8c8e0.svg"
+  },
+  {
+    "revision": "dc4d5293b8c62bcbea49e049856ac148",
+    "url": "/static/media/Silhouette.dc4d5293.svg"
+  },
+  {
+    "revision": "b0ec457f517f29a35a8cf88006b502cc",
+    "url": "/static/media/envelope.b0ec457f.svg"
+  },
+  {
+    "revision": "f7ee776412ec2655fcd18d4735a837cc",
+    "url": "/index.html"
+  }
+];
