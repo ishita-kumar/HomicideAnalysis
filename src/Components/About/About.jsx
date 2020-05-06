9cf05a12aa5b577c8b33eeb3e35d80b65b1db5a8
ishita-kumar <kumar.ishita@gmail.com>
2020-05-06 15:19:03 -0400

    stuff

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,10 +13,13 @@ const About = props => {
               rate for men aged 18 to 19 estimated at 46 per 100,000 – far higher than the risk faced by their peers in
               other regions. Firearms are also involved far more often in homicides in the Americas than in other parts
               of the world. Criminal activity is responsible for many more deaths worldwide than armed conflict and terrorism
-              combined. . By bringing together the available data, the research and analysis contained in the study can be used to learn, understand and strengthen prevention.
-              After World War II, crime rates increased in the United States, peaking from the 1980s to the early-1990s. Violent crime nearly quadrupled between 1960 and peaked around 1991. Since the 1990s, however, contrary to popular belief, crime in the United States has declined steadily. 
+              combined.After World War II, crime rates increased in the United States, peaking from the 1980s to the early-1990s. 
+              Violent crimes nearly quadrupled between 1960 and peaked around 1991. Since the 1990s, however, contrary to popular belief, 
+              crime in the United States has declined steadily.  By bringing together the available data, the research and analysis contained in the
+               study can be used to learn, understand and strengthen prevention.
+              
             </p>
-            <p>This study offers particular insights into the </p>
+ 
             <p>The study also provides analysis into Murders that have gone unknown</p>
           </div>
 
@@ -28,24 +31,23 @@ const About = props => {
             </p>
           </div>
          
-          {/* <div className="desc">
+          <div className="desc">
             <h4 className="subtitle">Visualizations </h4>
             <p>
-              ALLALALLALALA
+              <ul>
+                <li> State-Wise Depiction of Homicide</li>
+                <li> Crime Rate by Race</li>
+                <li> Gun Violence Crime over the years</li>
+                <li>Solved and Unsolved Homicides</li>
+                <li></li>
+                              </ul>
+           
             </p>
             <p>
               BLALALAL
             </p>
           </div>
-          <div className="desc">
-            <h4 className="subtitle"> Analysis</h4>
-            <p>
-              ANNANAN
-            </p>
-            <p>
-              ANNAN
-            </p>
-          </div> */}
+        
         </article>
       </div>
     </section>
